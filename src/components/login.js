7a047b42ee21e68c4e696799a15fd4e0ab50a9d7
Douglas Sturion <sturion.douglas@gmail.com>
2021-10-25 15:57:11 -0300

    Update

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,15 @@
-import { signInWithGoogle } from '../services/firebase';
+import { signInWithGoogle } from "../services/firebase";
 
-import '../App.css';
+import "../App.css";
 
 const Login = () => {
   return (
     <div>
-      <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
+      <button className="button" onClick={signInWithGoogle}>
+        <i className="fab fa-google"></i>Sign in with google
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
